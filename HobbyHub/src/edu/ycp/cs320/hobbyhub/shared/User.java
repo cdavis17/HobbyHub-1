41e6c451c6abf49749ee8ed4c1b6b8225a4c792c
Mfioravan <mfiroava@ycp.edu>
2014-03-14 11:16:18 -0400

    Added First/Last name

--- HobbyHub/src/edu/ycp/cs320/hobbyhub/shared/User.java
+++ HobbyHub/src/edu/ycp/cs320/hobbyhub/shared/User.java
@@ -6,6 +6,8 @@ public class User {
 	private String locationState;
 	private String password;
 	private String userEmail;
+	private String firstName;
+	private String lastName;
 	
 	public User() {
 		
@@ -16,7 +18,6 @@ public class User {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
 	public String getUserName() {
 		return userName;
 	}
@@ -26,7 +27,6 @@ public class User {
 	public void setLocationCity(String locationCity){
 		this.locationCity = locationCity;
 	}
-	
 	public String getLocationCity(){
 		return locationCity;
 	}
@@ -36,7 +36,6 @@ public class User {
 	public void setLocationState(String locationState){
 		this.locationState = locationState;
 	}
-	
 	public String getLocationState(){
 		return locationState;
 	}
@@ -46,7 +45,6 @@ public class User {
 	public void setPassword(String password){
 		this.password = password;
 	}
-	
 	public String getPassword(){
 		return password;
 	}
@@ -56,10 +54,25 @@ public class User {
 	public void setUserEmail(String email){
 		this.userEmail = email;
 	}
-	
 	public String getUserEmail(){
 		return userEmail;
 	}
 	
 	
+	// Set/Get first name of user
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	
+	// Set/Get last name of user
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	public String getLastName(){
+		return lastName;
+	}
 }
