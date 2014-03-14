688d96e548720bf187bfd33073ed69fd1866b054
Chris <cdavis17@ycp.edu>
2014-03-14 11:49:26 -0400

    Created a view class, add GWT features

---
+++ HobbyHub/src/edu/ycp/cs320/hobbyhub/view/MainView.java
@@ -0,0 +1,12 @@
+package edu.ycp.cs320.hobbyhub.view;
+
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.LayoutPanel;
+
+public class MainView {
+
+
+
+
+}
