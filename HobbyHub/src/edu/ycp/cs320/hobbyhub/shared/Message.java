438e4e9214003f0d7aa7d3b4e243b3bcd2d94727
Chris <cdavis17@ycp.edu>
2014-03-21 11:46:28 -0400

    Variables for Message class added

--- HobbyHub/src/edu/ycp/cs320/hobbyhub/shared/Message.java
+++ HobbyHub/src/edu/ycp/cs320/hobbyhub/shared/Message.java
@@ -1,5 +1,15 @@
 package edu.ycp.cs320.hobbyhub.shared;
 
 public class Message {
-
+	private User sender;
+	private User receiver;
+	private String subject;
+	private String body;
+	
+	// Constructor
+	public Message(){
+	
+	}
+	
+	
 }
