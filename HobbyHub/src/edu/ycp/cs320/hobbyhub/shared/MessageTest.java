fe719e3c0f1c6293077099e0d60fb1ef76671d77
Chris <cdavis17@ycp.edu>
2014-03-24 14:25:36 -0400

    Message test class
    Created messageTest class, all PASS

---
+++ HobbyHub/src/edu/ycp/cs320/hobbyhub/shared/MessageTest.java
@@ -0,0 +1,50 @@
+package edu.ycp.cs320.hobbyhub.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MessageTest {
+
+	Message tester;
+	String bodyText;
+	User sender;
+	User receiver;
+	
+	@Before
+	public void setup(){
+		tester = new Message();
+		bodyText = "This is the body of the message.";
+		sender = new User();
+		receiver = new User();
+	}
+	
+	//
+	@Test
+	public void testSetSubject(){
+		tester.setSubject("test");
+		assertEquals("test", "test", tester.getSubject());
+	}
+	
+	
+	@Test
+	public void testsetBody(){
+		tester.setBody(bodyText);
+		assertEquals(bodyText, tester.getBody());
+	}
+	
+	@Test
+	public void testsetSender(){
+		tester.setSender(sender);
+		assertEquals(sender, tester.getSender());
+	}
+	
+	
+	@Test
+	public void testsetReciever(){
+		tester.setReciever(receiver);
+		assertEquals(receiver, tester.getReciever());
+	}
+
+}
