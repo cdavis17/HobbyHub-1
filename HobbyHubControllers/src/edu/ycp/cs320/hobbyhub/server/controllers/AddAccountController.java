7693377c25e9643cda1b75a21b80ec87a334890d
Michael <mfiroava@ycp.edu>
2014-03-28 14:42:46 -0400

    Added addAccountController

---
+++ HobbyHubControllers/src/edu/ycp/cs320/hobbyhub/server/controllers/AddAccountController.java
@@ -0,0 +1,13 @@
+package edu.ycp.cs320.hobbyhub.server.controllers;
+
+import edu.ycp.cs320.hobbyhub.server.persist.DatabaseProvider;
+import edu.ycp.cs320.hobbyhub.server.persist.IDatabase;
+import edu.ycp.cs320.hobbyhub.shared.User;
+
+public class AddAccountController {
+	public void addAccount(User user) {
+		IDatabase db = DatabaseProvider.getInstance();
+		
+		// TODO: use db to add the account
+	}
+}
